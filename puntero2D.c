7d7f15baaa68a2f8ceede91c303808d1d43c877e
jserep00 <47887420+jserep00@users.noreply.github.com>
2019-04-25 19:34:52 +0200

    Update puntero2D.c
    arreglo al ejercicio

--- puntero2D.c
+++ puntero2D.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void rellena (int*matriz, int filas, int columnas);
 void pinta (int*matriz, int filas, int columnas);
-
 int main () {
 	int filas, columnas, *matriz;
-	printf ("Numero de filas: ");
-	scanf ("%d",&filas);
-	printf ("Numero de columnas: ");
-	scanf ("%d",&columnas);
-	matriz = (int*) malloc (filas*columnas*sizeof(int));
-	rellena (matriz,filas,columnas);
+	srand(time(NULL));
+	printf ("Escriba el numero de filas: ");
+	scanf ("%d", &filas);
+	printf ("Escriba el numero de columnas: ");
+	scanf ("%d", &columnas);
+	matriz=(int*)malloc(filas*columnas*sizeof(int));
+	rellena (matriz, filas, columnas);
 	pinta (matriz, filas, columnas);
-return 0;
+return 0;	
 }
 
-void rellena (int*matriz, int filas, int columnas){
-	int i,j;
-	for (i=0; i<filas; i++){
+void rellena (int*matriz, int filas, int columnas) {
+	int i, j;
+	for (i=0; i<filas; i++) {
 		for (j=0; j<columnas; j++) {
-			matriz [i*filas+j] = rand() %10;
+			matriz[i*filas+j]=rand() %10;
 		}
 	}
 }
-
-void pinta (int*matriz, int filas, int columnas){
-	int i,j;
-	for (i=0;i<filas; i++) {
-		for (j=0;j<columnas; j++) {
-			printf ("%d ", matriz [i*filas+j]);
+void pinta (int*matriz, int filas, int columnas) {
+	int i, j;
+	for (i=0; i<filas; i++) {
+		for (j=0; j<columnas; j++) {
+			printf ("%d\t", matriz[i*filas+j]);
 		}
 		printf ("\n");
-	}
+	}	
 }
